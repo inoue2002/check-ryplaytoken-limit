795766f25884e212a9f14a3318858a26abc884ce
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-01-24 11:57:52 +0900

    コメント

--- index.js
+++ index.js
@@ -24,11 +24,12 @@ exports.handler = (event) => {
         case "message":
           message = await messageFunc(event);
           break;
-        case "postback":
-          message = await postbackFunc(event);
-          break;
         case "follow":
-          message = { type: "text", text: "追加ありがとうございます！" };
+          message = {
+            type: "text",
+            text:
+              "秒数をトークに投げるとその期間たったあとに応答するよ。リプライトークンの期限がいつまでなのか調べるよ。1-100に対応しているよ。",
+          };
           break;
       }
       // メッセージを返信
@@ -56,12 +57,26 @@ async function sendFunc(replyToken, mes) {
 }
 
 async function messageFunc(event) {
+  const user_message = event.message.text;
+  const target_number = Number(user_message);
   let message = "";
+  //数値に変換できた場合
+  if (target_number !== NaN) {
+    if (target_number > 100) {
+      //100秒よりも大きい数字だった場合
+
+      message = { type: "text" };
+    } else {
+      //その時間待って応答する
+    }
+  } else {
+    //数値じゃなかった場合
+    message = {
+      type: "text",
+      text: "数値じゃないみたいだね。1-100までの数値を送信してくれるといいよ。",
+    };
+  }
+
   message = { type: "text", text: `メッセージイベント` };
   return message;
 }
-const postbackFunc = async function (event) {
-  let message = "";
-  message = { type: "text", text: "ポストバックイベント" };
-  return message;
-};
